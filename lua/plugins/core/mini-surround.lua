4d4257a1c6534e5050ed15e12b125722a9b7e5f2
Kevin Michael <kmichaels@L-5CG4443W7F.>
2025-01-20 10:17:40 +1300

    redundant entries in mini & fixing ruff

--- lua/plugins/core/mini-surround.lua
+++ lua/plugins/core/mini-surround.lua
@@ -4,12 +4,12 @@ return {
 		version = false,
 		opts = {
 			mappings = {
-				add = "msa", -- Add surrounding
-				delete = "msd", -- Delete surrounding
+				add = "ms", -- Add surrounding
+				delete = "md", -- Delete surrounding
+				replace = "mr", -- Replace surrounding
 				find = "sf", -- Find surrounding (to the right)
 				find_left = "sF", -- Find surrounding (to the left)
 				highlight = "sh", -- Highlight surrounding
-				replace = "msr", -- Replace surrounding
 				update_n_lines = "sn", -- Update `n_lines`
 			},
 		},
--- lua/plugins/core/mini.lua
+++ lua/plugins/core/mini.lua
@@ -6,9 +6,9 @@ return {
 	config = function()
 		require("mini.basics").setup({}) -- Basic settings
 		require("mini.move").setup({}) -- Move text/blocks
-		require("mini.ai").setup({}) -- Enhanced text objects
-		require("mini.surround").setup({}) -- Surround management
-		require("mini.comment").setup({}) -- Commenting
+		-- require("mini.ai").setup({}) -- Enhanced text objects
+		-- require("mini.surround").setup({}) -- Surround management
+		-- require("mini.comment").setup({}) -- Commenting
 		require("mini.clue").setup({
 			triggers = {
 				{ mode = "n", keys = "<Leader>" },
@@ -17,11 +17,11 @@ return {
 				{ mode = "x", keys = "m" },
 			},
 		}) -- Keymap hints
-		require("mini.files").setup({}) -- File tree
+		-- require("mini.files").setup({}) -- File tree
 		require("mini.starter").setup({}) -- Starting screen
 		require("mini.animate").setup({}) -- Smooth animations
-		require("mini.completion").setup({}) -- Completion
-		require("mini.align").setup({}) -- Alignment
+		-- require("mini.completion").setup({}) -- Completion
+		-- require("mini.align").setup({}) -- Alignment
 		require("mini.bufremove").setup({}) -- Buffer removal
 		require("mini.indentscope").setup({}) -- Indent scope visualization
 	end,
