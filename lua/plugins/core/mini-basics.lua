b30854820de6a54055b2d673aeefe6f69563843f
Kevin Michael <kmichaels@Kevins-MacBook-Air.local>
2025-07-13 11:02:36 +1200

    feat(mini-basics): Configure mini.basics for enhanced user experience
    This commit introduces a more refined configuration for the mini.basics plugin.
    
    Key changes include:
    - Enabling basic options while disabling extra UI elements and setting window borders to single.
    - Disabling basic mappings to allow for custom configurations, specifically disabling option toggles and window movement with Alt.
    - Enabling basic autocommands but disabling relative numbers in visual mode.
    - Setting silent mode to false to allow for feedback.
    - Disabling showmode, backup, and writebackup for a cleaner interface.
    - Enabling relative numbers for improved navigation.

--- lua/plugins/core/mini-basics.lua
+++ lua/plugins/core/mini-basics.lua
@@ -3,7 +3,29 @@ return {
 		"echasnovski/mini.basics",
 		version = false,
 		config = function()
-			require("mini.basics").setup()
+			require("mini.basics").setup({
+				options = {
+					basic = true,
+					extra_ui = false,
+					win_borders = "single",
+				},
+				mappings = {
+					basic = false,
+					option_toggle_prefix = "",
+					windows = false,
+					move_with_alt = false,
+				},
+				autocommands = {
+					basic = true,
+					relnum_in_visual_mode = false,
+				},
+				silent = false,
+			})
+
+			vim.opt.showmode = false
+			vim.opt.backup = false
+			vim.opt.writebackup = false
+			vim.opt.relativenumber = true
 		end,
 	},
 }
