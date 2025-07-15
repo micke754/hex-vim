a4050445b65b781dd9e4d0a5834d06465a13e16a
Kevin Michaels <kmichaels@L-5CG4443W7F.localdomain>
2025-07-15 21:48:57 +1200

    feat(completions): Add cmp-dbee for SQL completion
    Integrates cmp-dbee plugin for SQL autocompletion within Neovim's completion engine. This enhancement provides database-aware suggestions for SQL queries.

--- lua/plugins/core/completions.lua
+++ lua/plugins/core/completions.lua
@@ -5,6 +5,14 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
+			{
+				"MattiasMTS/cmp-dbee",
+				dependencies = {
+					{ "kndndrj/nvim-dbee" },
+				},
+				ft = "sql",
+				opts = {},
+			},
 		},
 		config = function()
 			local cmp = require("cmp")
@@ -17,11 +25,12 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				}),
-				sources = {
+				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "buffer" },
 					{ name = "path" },
-				},
+					{ name = "cmp-dbee" }, -- Added source for cmp-dbee
+				}),
 			})
 		end,
 	},
